e12f93feccc648a5c470358533df551e5aeefeb9
Vito Mule <mulevito@gmail.com>
2015-01-19 22:26:24 +0000

    updated Debugger receipt with PkgCreator processor.

--- AdobeFlashDebugger/AdobeFlashDmgUnpacker.py
+++ AdobeFlashDebugger/AdobeFlashDmgUnpacker.py
@@ -92,7 +92,7 @@ class AdobeFlashDmgUnpacker(Processor):
                                                     "finalize")
             with open(original_postflight_path, "rb") as fref:
                 postflight = fref.read()
-            patched_postflight_path = os.path.join(temp_path, "postflight")
+            patched_postflight_path = os.path.join(temp_path, "finalize")
             with open(patched_postflight_path, "wb") as fref:
                 fref.write(postflight.replace(
                     "/Library/Internet Plug-Ins", temp_path))
@@ -113,34 +113,8 @@ class AdobeFlashDmgUnpacker(Processor):
 
             shutil.rmtree(os.path.join(pkgroot, 'Library/PreferencePanes'))
 
-            cache_path = os.path.join('/Users/vmule/Library/AutoPkg/Cache',
-                'com.github.autopkg.munki.FlashDebuggerExtractPackage')
-            unpkg_path = os.path.join(cache_path,
-                'Payload/AdobeFlashPlayerComponent.pkg')
-
-            os.remove(os.path.join(unpkg_path, 'Payload'))
-            cmd = ( '/usr/bin/ditto', '-z', '-c', pkgroot + '/', unpkg_path + '/Payload')
-            subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            os.remove(unpkg_path + '/Bom')
-            cmd = ('/usr/bin/mkbom', pkgroot + '/',  unpkg_path + '/Bom')
-            subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-
-            pkg_file_num = str(sum([len(files+d) for r, d, files in os.walk(pkgroot)]) + 1)
-            pkg_size = str(int(subprocess.check_output(['du','-sk', pkgroot]).split()[0]) - 4)
-            pkg_line = "<payload numberOfFiles=\"" + pkg_file_num + "\" installKBytes=\"" + pkg_size +"\"/>"
-
-            for line in fileinput.input(unpkg_path + '/PackageInfo', inplace=True):
-                line = re.sub(r"<payload numberOfFiles=\".*\" installKBytes=\".*\"/>", pkg_line, line.rstrip())
-                print line
-
-
         finally:
             shutil.rmtree(temp_path)
-
-        pkg_path = os.path.join(cache_path, 'AdobeFlashDebugger.pkg')
-        cmd = ('pkgutil', '--flatten', unpkg_path, pkg_path)
-        subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
     def main(self):
         try:
